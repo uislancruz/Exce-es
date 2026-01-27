887284421b61649b3a42261ace55c3dc91b9f093
Uislan Cruz <uislansk8@hotmail.com>
2026-01-26 22:57:33 -0300

    Lançando uma exceção ppara produtos inativo, aqui o exemplo com erro devido a não ativar ele

--- src/Principal.java
+++ src/Principal.java
@@ -3,10 +3,11 @@ import com.algaworks.estoque.Produto;
 public class Principal {
 
     static void main(String[] args) {
+
         Produto produto = new Produto("Apple Watch");
         produto.adicionarEstoque(10);
 
-        produto.retirarEstoque(-5);
+        produto.retirarEstoque(5);
 
         System.out.printf("Estoque: %d%n", produto.getQuantidadeEstoque());
     }
