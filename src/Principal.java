e10eba8a558534a4b655955d10448cee8e8910cc
Uislan Cruz <uislansk8@hotmail.com>
2026-01-26 22:28:45 -0300

    Lançando exceções

---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+import com.algaworks.estoque.Produto;
+
+public class Principal {
+
+    static void main(String[] args) {
+        Produto produto = new Produto("Apple Watch");
+        produto.adicionarEstoque(10);
+
+        produto.retirarEstoque(5);
+
+        System.out.printf("Estoque: %d%n", produto.getQuantidadeEstoque());
+    }
+}
