b2dd3a332c4018f6f93913a75fff05caff8c429f
Uislan Cruz <uislansk8@hotmail.com>
2026-04-09 14:39:45 -0300

    Criando classes de exceçcoes para tomar mais limpo o codigo e mais visivel para quem ler

--- src/com/algaworks/excecoes/Principal.java
+++ src/com/algaworks/excecoes/Principal.java
@@ -1,4 +1,18 @@
 package com.algaworks.excecoes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Principal {
+
+    static void main(String[] args) {
+        try {
+            Path arquivo = Path.of("C:\\Users\\Uislan\\OneDrive\\Documentos\\AulaExcecoes\\teste.txt");
+            Files.createFile(arquivo);
+        } catch (IOException e) {
+            System.out.println("Erro ao criar arquivo" + e.getMessage());
+        }
+
+    }
 }
